8dddbef89174418141d978569cda14b171100157
Alexander Anpleenko <vaeum@yahoo.com>
2017-04-21 01:14:42 +0300

    fix init

--- src/scripts/jInvertScroll.js
+++ src/scripts/jInvertScroll.js
@@ -1,5 +1,3 @@
-$.jInvertScroll(['.scrollable']);
-
 const blockWidth = 423;
 const blockHeight = 220;
 const wrapPaddingLeft = 160;
@@ -9,20 +7,22 @@ const blockEl = $('.scrollable__block');
 const wrapEl = $('.scrollable__wrap');
 
 targetEl.css({
-  width: (blockEl.length * blockWidth) + wrapPaddingLeft + wrapPaddingRight
-})
+  width: (blockEl.length * blockWidth) + wrapPaddingLeft + wrapPaddingRight,
+});
 
 wrapEl.each((i, e) => {
   $(e).css({
-    left: (i * blockWidth) + wrapPaddingLeft
-  })
-})
+    left: (i * blockWidth) + wrapPaddingLeft,
+  });
+});
 
 blockEl.css({
   height: blockHeight,
   width: blockWidth,
-})
+});
 
 wrapEl.css({
   height: (blockHeight * 2) + 20,
-})
+});
+
+$.jInvertScroll(['.scrollable']);
