692840e572e34117285ec56d945739675b691cf6
Alexander Anpleenko <vaeum@yahoo.com>
2017-04-27 11:46:13 +0300

    fix touch event

--- src/scripts/touch.js
+++ src/scripts/touch.js
@@ -1,23 +1,25 @@
-const jqueryEl = $('#scrollable');
-const elemWidth = jqueryEl.width();
-let widthWithOutWindowsWidth = elemWidth - $(window).width();
+if (isMobile.any) {
+  const jqueryEl = $('#scrollable');
+  const elemWidth = jqueryEl.width();
+  let widthWithOutWindowsWidth = elemWidth - $(window).width();
 
-$(window).on('resize', () => {
-  widthWithOutWindowsWidth = elemWidth - $(window).width();
-});
+  $(window).on('resize', () => {
+    widthWithOutWindowsWidth = elemWidth - $(window).width();
+  });
 
-let margin = 0;
-new Hammer(jqueryEl[0], { domEvents: true });
+  let margin = 0;
+  new Hammer(jqueryEl[0], { domEvents: true });
 
-jqueryEl.on('panstart', () => {
-  margin = parseInt(jqueryEl.css('left'), 10);
-});
+  jqueryEl.on('panstart', () => {
+    margin = parseInt(jqueryEl.css('left'), 10);
+  });
 
-jqueryEl.on('pan', (e) => {
-  const delta = margin + e.originalEvent.gesture.deltaX;
-  if (delta >= -widthWithOutWindowsWidth && delta <= 0) {
-    jqueryEl.css({
-      left: margin + e.originalEvent.gesture.deltaX,
-    });
-  }
-});
+  jqueryEl.on('pan', (e) => {
+    const delta = margin + e.originalEvent.gesture.deltaX;
+    if (delta >= -widthWithOutWindowsWidth && delta <= 0) {
+      jqueryEl.css({
+        left: margin + e.originalEvent.gesture.deltaX,
+      });
+    }
+  });
+}
