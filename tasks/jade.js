72c9692fea123d37bbb03df2e972af335f5a5de5
Alexander Anpleenko <vaeum@yahoo.com>
2017-05-10 22:27:30 +0300

    fix jade task

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,6 +1,5 @@
 import gulp from 'gulp';
 import { config, $, bs, errorLogFunc, isDev, isProd } from './config';
-import data from '../src/data';
 
 const utils = {
   addZeroToNumber(num) {
@@ -11,11 +10,11 @@ const utils = {
 gulp.task('jade', () =>
   gulp.src([config.src.jade])
     .pipe($.jade({
-      locals: { ...data, DEV: isDev, PROD: isProd, ...utils },
+      locals: { ...require('../src/data').default, DEV: isDev, PROD: isProd, ...utils },
     })).on('error', errorLogFunc)
 
     .pipe($.posthtml(config.POSTHTML_PROCESSORS))
     .pipe($.if(isDev, $.jsbeautifier(config.jsbeautifierConfig)))
     .pipe(gulp.dest(config.dest.app))
-    .on('end', bs.reload)
+    .on('end', bs.reload),
 );
