11d3d3c76f14c4cad8fddd39a330ccba02001218
Pavel <19paul91@mail.ru>
2017-05-25 22:36:30 +0300

    add typed plugin

--- src/scripts/aboutPage.js
+++ src/scripts/aboutPage.js
@@ -13,4 +13,13 @@ $(() => {
   // });
   //
   // $('.viewport-checker').viewportChecker();
+
+  $(".about__typed-text").typed({
+    strings: ["WEB DESIGNER", "WEB DEVELOPER", "DOG LOVER", "COFFEE DRINKER"],
+    typeSpeed: 50,
+    backSpeed: 20,
+    backDelay: 2000,
+    showCursor: true,
+    loop: true,
+  });
 });
