f1fd09701af4473094d6934df0711e03a161205b
Pavel <19paul91@mail.ru>
2017-05-25 22:53:08 +0300

    fix about page

--- src/scripts/aboutPage.js
+++ src/scripts/aboutPage.js
@@ -14,8 +14,8 @@ $(() => {
   //
   // $('.viewport-checker').viewportChecker();
 
-  $(".about__typed-text").typed({
-    strings: ["WEB DESIGNER", "WEB DEVELOPER", "DOG LOVER", "COFFEE DRINKER"],
+  $('.about__typed-text').typed({
+    strings: ["WEB DESIGNER.", "WEB DEVELOPER.", "DOG LOVER.", "COFFEE DRINKER."],
     typeSpeed: 50,
     backSpeed: 20,
     backDelay: 2000,
